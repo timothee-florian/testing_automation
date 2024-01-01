fd0ed14cfde76a9774d325ab9bf8c0c297fdc189
timothee-florian <timothee.florian.bronner@gmail.com>
2024-01-01 17:34:09 +0100

    debug typo on function name

--- tests/test_importing.py
+++ tests/test_importing.py
@@ -4,15 +4,15 @@ from importing import sub_numbers
 class TestMyModule(unittest.TestCase):
 
     def test_sub_numbers(self):
-        result = add_numbers(2, 3)
+        result = sub_numbers(2, 3)
         self.assertEqual(result, -1)
 
     def test_sub_numbers_negative(self):
-        result = add_numbers(-2, 3)
+        result = sub_numbers(-2, 3)
         self.assertEqual(result, -4)
 
     def test_sub_numbers_zero(self):
-        result = add_numbers(0, 0)
+        result = sub_numbers(0, 0)
         self.assertEqual(result, 0)
 
 if __name__ == '__main__':
